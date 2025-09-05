23b1ad540c21ea3da9ed563c7c0468a384d0db89
achernushevichsacha-web <achernushevich@list.ru>
2025-09-05 21:17:10 +0300

    sec commit

--- SRC/Text.java
+++ SRC/Text.java
@@ -3,6 +3,10 @@ public class Text {
         System.out.println("Hello World");
         int b = 8;
         int c = 17;
-        int а = 4*(b+c-1)/2;
+        int a;
+        System.out.println( "Задача 1 a = " + 4*(b+c-1)/2);
+        int n = 56;
+        int s = n%10;
+        int e = (n/10)%10;
+        System.out.println("Задача 2 n = " + (s+e));}
     }
-}
